05f0524427764b5139d8b148218af2ae99b02caa
BaekJinH <tmdfyd6868@naver>
2022-04-22 15:32:37 +0900

    1

---
+++ js/sub.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('.glass img:nth-of-type(1)').click(function(){
+        $('img:nth-of-type(1)').show();
+    });
+});
+
+    $("love").click(function(){
+        toggleClass(".active-color")
+    });
+// $(function() {
+            
+//     $(".p_btn").click(function() {
+//         var $this = $(this);
+//         var target = $this.prev();
+//         var num = parseInt(target.text());
+//         num++;
+
+//         target.text(num);
+//     });
+
+//     $(".m_btn").click(function() {
+//         var $this = $(this);
+//         var target = $this.next();
+//         var num = parseInt(target.text());
+//         num -= ( (num === 1) ? 0 : 1 );
+
+//         target.text(num);
+//     });
+// });
