5bff781d16f39102ad3039ea89a615cc6613e941
BaekJinH <tmdfyd6868@naver>
2022-04-26 18:01:34 +0900

    sub_plus

--- js/sub.js
+++ js/sub.js
@@ -1,30 +1,43 @@
 $(function(){
-    $('.glass img:nth-of-type(1)').click(function(){
-        $('img:nth-of-type(1)').show();
+    $('.glass > button:nth-of-type(1)').click(function(){
+        $('#one').fadeIn();
+        $('#two').hide();
+        $('#three').hide();
     });
-
-
-    $(".button").click(function(){
-        toggleClass(".active-color")
+    $('.glass > button:nth-of-type(2)').click(function(){
+        $('#two').fadeIn();
+        $('#one').hide();
+        $('#three').hide();
+    });
+    $('.glass > button:nth-of-type(3)').click(function(){
+        $('#three').fadeIn();
+        $('#one').hide();
+        $('#two').hide();
+    });
+})
+$(function(){
+    $('.container > button:nth-of-typeof(1)').click(function(){
+        $()
+        $()
+        $()
+        $()
+    });
+    $('.container > button:nth-of-typeof(2)').click(function(){
+        $()
+        $()
+        $()
+        $()
+    });
+    $('.container > button:nth-of-typeof(3)').click(function(){
+        $()
+        $()
+        $()
+        $()
+    });
+    $('.container > button:nth-of-typeof(4)').click(function(){
+        $()
+        $()
+        $()
+        $()
     });
 });
-    // $(function() {
-            
-//     $(".p_btn").click(function() {
-//         var $this = $(this);
-//         var target = $this.prev();
-//         var num = parseInt(target.text());
-//         num++;
-
-//         target.text(num);
-//     });
-
-//     $(".m_btn").click(function() {
-//         var $this = $(this);
-//         var target = $this.next();
-//         var num = parseInt(target.text());
-//         num -= ( (num === 1) ? 0 : 1 );
-
-//         target.text(num);
-//     });
-// });
